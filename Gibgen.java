ae76fa74b8a678960c61d3ee8cbefa3380ed89ca
findc <findcal22@gmail.com>
2017-05-02 07:03:45 -0600

    Added conversion method stub

--- Gibgen.java
+++ Gibgen.java
@@ -2,28 +2,67 @@
  * White Whale Internship Coding Challenge Solution.
  * Created by Calvin Findley.
  */
-
 import java.util.Scanner;
+import java.util.Random;
 import java.io.*;
 
 public class Gibgen
 {
-    public static  void  main(String[] args)
+    public static void main(String[] args)
     {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Please Enter the Complete Filename of the Textfile You Would Like Transformed Into Gibberish: ");
+        String fileName = reader.nextLine();
+        String sentence = null;
+
         try
         {
-            BufferedReader inputStream = new BufferedReader(new FileReader("sample.txt"))
-            String line = inputStream.readLine();
-            System.out.println(line);
-            inputStream.close();
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            sentence = bufferedReader.readLine();
+            bufferedReader.close();
         }
-        catch(FileNotDoundException e)
+        
+        catch (FileNotFoundException ex)
         {
-            System.out.println("The file was not found or could not be opened");
+            System.out.println("File '" + fileName + "' was not found");
         }
-        catch(IOException e)
+
+        catch (IOException ex)
         {
-            System.out.println("Error reading from file");
+            System.out.println("Error reading from '" + fileName + "'");
+        }
+
+        preliminaryConversion(sentence);
+
+    }
+
+
+    public static void preliminaryConversion(String sentence)
+    {
+        System.out.println(sentence);
+        String[] tokens = sentence.split("\\W"); //splits on any non-word character
+        for (int i = 0; i < tokens.length; i++)
+        {
+            if(tokens[i].length() > 5)
+            {
+
+            }
+            else if(tokens[i].length() == 4)
+            {
+
+            }
+            else if(tokens[i].length() == 5)
+            {
+
+            }
+            else
+            {
+
+            }
         }
     }
+
+
+
 }
