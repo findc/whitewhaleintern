980021f596811d734e156fb2350ff232773e2389
findc <findcal22@gmail.com>
2017-05-02 06:03:41 -0600

    file chooser added

--- Gibgen.java
+++ Gibgen.java
@@ -13,6 +13,20 @@ public class Gibgen
 {
     public static  void  main(String[] args)
     {
-
+        try
+        {
+            BufferedReader inputStream = new BufferedReader(new FileReader("sample.txt"))
+            String line = inputStream.readLine();
+            System.out.println(line);
+            inputStream.close();
+        }
+        catch(FileNotDoundException e)
+        {
+            System.out.println("The file was not found or could not be opened");
+        }
+        catch(IOException e)
+        {
+            System.out.println("Error reading from file");
+        }
     }
 }
